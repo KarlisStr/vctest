96b9c9b28f231e41764ced884d65bb2527090b0b
Karlis Strautnieks <karlisstrautnieks2004@gmail.com>
2024-12-19 16:03:25 +0200

    Initial commit

---
+++ ks23147main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from ks23147libadd import *
+from ks23147libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
